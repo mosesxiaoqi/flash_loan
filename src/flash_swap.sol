1700efab3256fb03593321efed8ffbbdd45b03a6
Jonas <>
2025-03-28 20:52:01 +0800

    完成uniswapV2Call逻辑，实现闪电贷套利

--- src/flash_swap.sol
+++ src/flash_swap.sol
@@ -45,12 +45,22 @@ contract FlashSwap is IUniswapV2Callee, Ownable {
 
         // 由于token的地址在创建交易对时已经排序，所以不用在读取交易对里的token0和token1
         // 直接判断两个token的大小来决定uint256 amount0, uint256 amount1
+        /*
+            1. **动态分配**：
+            - token0 和 token1 的角色不是由用户调用 `createPair(tokenA, tokenB)` 时参数的顺序决定的
+            - 而是由代币合约地址的大小比较后自动确定的
+
+            2. **字典序比较**：
+            - 系统会比较两个代币地址的十六进制值
+            - 地址值较小的代币被指定为 token0
+            - 地址值较大的代币被指定为 token1
+        */
         (uint256 amount0, uint256 amount1) = tokenA < tokenB
             ? (amountA, amountB)
             : (amountB, amountA);
 
         // 传递滑点参数
-        bytes memory hookCallData = abi.encode(amountOutMin);
+        bytes memory hookCallData = abi.encode(pair2);
 
         IUniswapV2Pair(pair1).swap(
             amount0,
@@ -66,7 +76,51 @@ contract FlashSwap is IUniswapV2Callee, Ownable {
         uint amount1,
         bytes calldata data
     ) external {
-        // do something
+        /*
+            amount0和amount1代表的是token0和token1的数量
+            我们的业务是从pair1中取出tokenA，然后将tokenA转移到pair2中
+            所以amount0和amount1其中不是0的一个就是tokenA的数量
+            所以我们需要通过判断amount0和amount1哪一个不是0来确定tokenA和tokenB的地址
+            计算出要还的tokenB的数量和计算出兑换pair2池子中tokenB的数量
+            判断是否有利润
+            uniswap采用乐观模式，所以是先转移tokenA给pair2，然后再swap tokenB
+            将tokenB还给pair1
+            将利润转移给owner
+        */
+        require(amount0 > 0 || amount1 > 0, 'amount not 0!' )
+        address pair1 = msg.sender;
+        address pair2 = abi.decode(data, (address));
+        // 都是uniswap v2 的交易对合约地址
+        // 所以获取的token0和token1的地址顺序是一样的
+        address token0 = IUniswapV2Pair(pair1).token0();
+        address token1 = IUniswapV2Pair(pair1).token1();
+
+        // (address tokenA, address tokenB) = token0 < token1 ? (token0, token1) : (token1, token0);
+        (uint amountA, uint amountB )= amount0 != 0 ? (amount0, amount1) : (amount1, amount0);
+        (address tokenA, address tokenB) = amount0 != 0 ? (token0, token1) : (token1, token0);
+
+        // 这里获取的池子总量是以及发生了tokenA的转移后，那获取的是转移后的池子总量，这不是有问题吗？
+        // 其实这里获取的不是转移后的池子总量，而是代表交易对上次更新时记录的代币数量
+        // 只在交易结束时更新，看v2的swap函数会发现，总量就是在交易结束时将代币的余额更新
+        // 所以这里获取的不是实际池子的数量
+        (uint reserve0, uint reserve1, ) = IUniswapV2Pair(pair1).getReserves();
+        (uint reserveIn, uint reserveOut) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
+        uint amountRequired = UniswapV2Library.getAmountIn(amountA, reserveIn, reserveOut);
+
+        IERC20(tokenA).transfer(pair2, amountA);
+        (uint reserve0, uint reserve1, ) = IUniswapV2Pair(pair2).getReserves();
+        (uint reserveIn, uint reserveOut) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
+        uint amountReceived = UniswapV2Library.getAmountOut(amountA, reserveIn, reserveOut);
+        require(amountReceived > amountRequired, 'amountReceived < amountRequired');
+        IUniswapV2Pair(pair2).swap(amountA, amountB, address(this), new bytes(0));
+
+        IERC20(tokenB).transfer(pair1, amountRequired);
+
+        // 将利润（剩余的 tokenB）转移给调用者
+        uint profit = IERC20(tokenB).balanceOf(address(this));
+        IERC20(tokenB).transfer(owner, profit);
+
+
     }
 
     function withdraw() external {
--- src/libraries/UniswapV2Library.sol
+++ src/libraries/UniswapV2Library.sol
@@ -1,34 +1,43 @@
 pragma solidity >=0.5.0;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
-import './SafeMath.sol';
+import "./SafeMath.sol";
 
 library UniswapV2Library {
     using SafeMath for uint;
 
     // returns sorted token addresses, used to handle return values from pairs sorted in this order
-    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+    function sortTokens(
+        address tokenA,
+        address tokenB
+    ) internal pure returns (address token0, address token1) {
         // 判断两个token地址是否相同
-        require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
+        require(tokenA != tokenB, "UniswapV2Library: IDENTICAL_ADDRESSES");
         // 比较两个token地址的大小，小的地址作为token0，大的地址作为token1
-        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        (token0, token1) = tokenA < tokenB
+            ? (tokenA, tokenB)
+            : (tokenB, tokenA);
         // 判断token0是否为0地址
         // token0小于token1，如果token0不为0地址，则两个token地址都不为0地址
-        require(token0 != address(0), 'UniswapV2Library: ZERO_ADDRESS');
+        require(token0 != address(0), "UniswapV2Library: ZERO_ADDRESS");
     }
 
     // calculates the CREATE2 address for a pair without making any external calls
-    function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
+    function pairFor(
+        address factory,
+        address tokenA,
+        address tokenB
+    ) internal pure returns (address pair) {
         (address token0, address token1) = sortTokens(tokenA, tokenB);
         pair = address(
             uint(
                 keccak256(
                     abi.encodePacked(
-                        hex'ff',
+                        hex"ff",
                         factory,
                         keccak256(abi.encodePacked(token0, token1)),
-                        hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                        hex"96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f" // init code hash
                     )
                 )
             )
@@ -45,33 +54,112 @@ library UniswapV2Library {
         (address token0, ) = sortTokens(tokenA, tokenB);
         // 使用 `pairFor` 函数计算代币对的交易对合约地址
         // 调用交易对的 `getReserves()` 函数获取当前储备量
-        (uint reserve0, uint reserve1, ) = IUniswapV2Pair(pairFor(factory, tokenA, tokenB)).getReserves();
-        (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
+        (uint reserve0, uint reserve1, ) = IUniswapV2Pair(
+            pairFor(factory, tokenA, tokenB)
+        ).getReserves();
+        (reserveA, reserveB) = tokenA == token0
+            ? (reserve0, reserve1)
+            : (reserve1, reserve0);
     }
 
     // given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
-    function quote(uint amountA, uint reserveA, uint reserveB) internal pure returns (uint amountB) {
-        require(amountA > 0, 'UniswapV2Library: INSUFFICIENT_AMOUNT');
-        require(reserveA > 0 && reserveB > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
+    function quote(
+        uint amountA,
+        uint reserveA,
+        uint reserveB
+    ) internal pure returns (uint amountB) {
+        require(amountA > 0, "UniswapV2Library: INSUFFICIENT_AMOUNT");
+        require(
+            reserveA > 0 && reserveB > 0,
+            "UniswapV2Library: INSUFFICIENT_LIQUIDITY"
+        );
         amountB = amountA.mul(reserveB) / reserveA;
     }
 
+    // `amountIn`: 输入的代币数量
+    // `reserveIn`: 交易对中输入代币的储备量
+    // `reserveOut`: 交易对中输出代币的储备量
     // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
-    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
-        require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
-        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
+    function getAmountOut(
+        uint amountIn,
+        uint reserveIn,
+        uint reserveOut
+    ) internal pure returns (uint amountOut) {
+        require(amountIn > 0, "UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT");
+        require(
+            reserveIn > 0 && reserveOut > 0,
+            "UniswapV2Library: INSUFFICIENT_LIQUIDITY"
+        );
+        // 理论公式： (reserveIn + amountIn * 0.997) * (reserveOut - amountOut) = reserveIn * reserveOut
+        // 推导过程： (reserveOut - amountOut) = (reserveIn * reserveOut) / (reserveIn + amountIn * 0.997)
+        // 推导过程： amountOut = reserveOut - (reserveIn * reserveOut) / (reserveIn + amountIn * 0.997)
+        // 推导过程： amountOut = (reserveOut*(reserveIn + amountIn * 0.997))/(reserveIn + amountIn * 0.997) - (reserveIn * reserveOut) / (reserveIn + amountIn * 0.997)
+        // 推导过程： amountOut = (reserveOut*(reserveIn + amountIn * 0.997) - (reserveIn * reserveOut)) / (reserveIn + amountIn * 0.997)
+        // 推导过程： amountOut = (reserveOut*reserveIn + reserveOut*amountIn*0.997 - reserveIn * reserveOut) / (reserveIn + amountIn * 0.997)
+        // 推导过程： amountOut = (reserveOut*amountIn*0.997) / (reserveIn + amountIn * 0.997)
+        // 推导过程： amountOut = (reserveOut*amountIn*997) / (reserveIn*1000 + amountIn * 997)
+        // 推导过程： 乘1000是为了减少精度损失，因为amountIn是用户输入的，所以可能是小数
+        // 推导过程： 所以分子就是reserveOut.mul(amountIn).mul(997)
+        // 推导过程： 所以分母就是reserveIn.mul(1000).add(amountIn.mul(997))
+        /**
+            1. **避免浮点数运算**：
+            - 以太坊智能合约不支持浮点数运算
+            - 直接使用 0.997 这样的小数是不可能的
+
+            2. **分数表示**：
+            - 手续费率 0.3% 等同于 3/1000
+            - 保留率（扣除手续费后的比例）为 (1000-3)/1000 = 997/1000
+
+            3. **整数运算中保持精度**：
+            - 乘以 1000 然后用 997 进行相应计算，可以在整数环境中精确表达这个比例
+            - 这种分子/分母的处理方式避免了浮点数计算的需求
+        */
         uint amountInWithFee = amountIn.mul(997);
         uint numerator = amountInWithFee.mul(reserveOut);
         uint denominator = reserveIn.mul(1000).add(amountInWithFee);
         amountOut = numerator / denominator;
     }
 
+    // `amountOut`: 希望获得的输出代币数量
+    // `reserveIn`: 交易对中输入代币的储备量
+    // `reserveOut`: 交易对中输出代币的储备量
     // given an output amount of an asset and pair reserves, returns a required input amount of the other asset
-    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) internal pure returns (uint amountIn) {
-        require(amountOut > 0, 'UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT');
-        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
+    function getAmountIn(
+        uint amountOut,
+        uint reserveIn,
+        uint reserveOut
+    ) internal pure returns (uint amountIn) {
+        // 希望获得的输出代币数量必须大于0
+        require(amountOut > 0, "UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT");
+        // 交易对中输入代币的储备量和输出代币的储备量都必须大于0
+        require(
+            reserveIn > 0 && reserveOut > 0,
+            "UniswapV2Library: INSUFFICIENT_LIQUIDITY"
+        );
+        // 由于交易存在千三手续费，
+        // 而手续费是随着交易一块进入了交易对的储备量
+        // 所以在计算时要去掉这部分手续费
+        // 千三就是0.003，所以总量的0.997就是去掉千三手续费后我们需要的数量
+        // 注意⚠️：只有存入有手续费，提取没有
+        // 理论公式：(reserveIn + amountIn * 0.997) * (reserveOut - amountOut) = reserveIn * reserveOut
+        // 推导过程： reserveIn + amountIn * 0.997 = (reserveIn * reserveOut) / (reserveOut - amountOut)
+        // 推导过程： amountIn * 0.997 = (reserveIn * reserveOut) / (reserveOut - amountOut) - reserveIn
+        // 推导过程： reserveIn = reserveIn*1，所以将reserveIn*{(reserveOut - amountOut)/(reserveOut - amountOut)}，同时扩大分子分母
+        // 推导过程： amountIn * 0.997 = (reserveIn * reserveOut - reserveIn*(reserveOut - amountOut)) / (reserveOut - amountOut)
+        // 推导过程： 接下来提取reserveIn
+        // 推导过程： amountIn * 0.997 = (reserveIn*(reserveOut - (reserveOut - amountOut))) / (reserveOut - amountOut)
+        // 推导过程： amountIn * 0.997 = (reserveIn*amountOut) / (reserveOut - amountOut)
+        // 推导过程： 同时将两边*1000
+        // 推导过程： amountIn * 997 = (reserveIn*amountOut*1000) / (reserveOut - amountOut)
+        // 推导过程： 除以997，就是*997分之1，就得到了amountIn
+        // 推导过程： amountIn = (reserveIn*amountOut*1000) / （(reserveOut - amountOut) * 997）
+        // 推导过程： 所以分母就是reserveIn*amountOut*1000，代码就是reserveIn.mul(amountOut).mul(1000)
+        // 推导过程： 所以分子就是(reserveOut - amountOut) * 997，代码就是reserveOut.sub(amountOut).mul(997)
         uint numerator = reserveIn.mul(amountOut).mul(1000);
         uint denominator = reserveOut.sub(amountOut).mul(997);
+        // 1. Solidity中的整数除法会自动截断小数部分（向下取整）
+        // 2. 然后代码添加1，有效地将结果向上取整
+        // 向下取整会导致amountIn变小，期望获得的输出代币数量不足，所以需要向上取整
         amountIn = (numerator / denominator).add(1);
     }
 
@@ -81,11 +169,15 @@ library UniswapV2Library {
         uint amountIn,
         address[] memory path
     ) internal view returns (uint[] memory amounts) {
-        require(path.length >= 2, 'UniswapV2Library: INVALID_PATH');
+        require(path.length >= 2, "UniswapV2Library: INVALID_PATH");
         amounts = new uint[](path.length);
         amounts[0] = amountIn;
         for (uint i; i < path.length - 1; i++) {
-            (uint reserveIn, uint reserveOut) = getReserves(factory, path[i], path[i + 1]);
+            (uint reserveIn, uint reserveOut) = getReserves(
+                factory,
+                path[i],
+                path[i + 1]
+            );
             amounts[i + 1] = getAmountOut(amounts[i], reserveIn, reserveOut);
         }
     }
@@ -98,7 +190,7 @@ library UniswapV2Library {
     ) internal view returns (uint[] memory amounts) {
         // path数组是交易路径，表达path[0] → path[1] → ... → path[path.length - 1]
         // path数组的长度至少为2
-        require(path.length >= 2, 'UniswapV2Library: INVALID_PATH');
+        require(path.length >= 2, "UniswapV2Library: INVALID_PATH");
         // 创建一个长度为path.length的数组
         amounts = new uint[](path.length);
         // 最后一个元素的值为amountOut
@@ -106,7 +198,11 @@ library UniswapV2Library {
         amounts[amounts.length - 1] = amountOut;
         for (uint i = path.length - 1; i > 0; i--) {
             // 获取交易对中两个代币的当前储备量
-            (uint reserveIn, uint reserveOut) = getReserves(factory, path[i - 1], path[i]);
+            (uint reserveIn, uint reserveOut) = getReserves(
+                factory,
+                path[i - 1],
+                path[i]
+            );
             amounts[i - 1] = getAmountIn(amounts[i], reserveIn, reserveOut);
         }
     }
